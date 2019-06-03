07aeded7e716bcfc39912e2df713813a17e9e598
Karl Xu <karl.yogi.xu@gmail.com>
2019-06-04 05:01:40 +0800

    deploy nodejs app to k8s

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000
 
 const host = 'somehost'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! This is a nodejs app.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
