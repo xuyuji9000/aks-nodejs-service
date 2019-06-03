304446f5962b79ba95be084d304f110fe949e690
Karl Xu <karl.yogi.xu@gmail.com>
2019-06-04 05:37:33 +0800

    deploy new version

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # Reference 
 
 1. [Continuous delivery to Kubernetes with Travis CI](https://caveofcode.com/2017/03/continuous-delivery-to-kubernetes-with-travis-ci/amp/)
+
+2. [CI/CD with Kubernetes and Travis CI](https://codygreen.com/2018/08/13/ci-cd-with-kubernetes-and-travis-ci/)
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World! This is a nodejs app v2.'))
+app.get('/', (req, res) => res.send('Hello World! This is a nodejs app v4.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
