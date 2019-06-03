6511c53a54add4d9050951223e010dc303644135
Karl Xu <karl.yogi.xu@gmail.com>
2019-06-04 05:26:17 +0800

    skaffold.yaml for dev

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const host = 'somehost'
-
-app.get('/', (req, res) => res.send('Hello World! This is a nodejs app.'))
+app.get('/', (req, res) => res.send('Hello World! This is a nodejs app v2.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
